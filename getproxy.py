8e68de1cd99de280b9b2066e315f7de07309008c
BreakingTechFr <breakingtechfr@gmail.com>
2024-08-22 21:18:33 +0200

    up

--- getproxy.py
+++ getproxy.py
@@ -2,8 +2,10 @@ import json
 import datetime
 import requests
 import re
+import time
+from requests.exceptions import ConnectionError, HTTPError
 
-class Downloadproxies():
+class DownloadProxies:
     def __init__(self) -> None:
         self.api = {
             'socks4': [
@@ -35,45 +37,40 @@ class Downloadproxies():
             ]
         }
         self.proxy_dict = {'socks4': [], 'socks5': [], 'http': []}
-        pass
 
     def get_special1(self):
         proxy_list = []
         try:
             r = requests.get("https://www.socks-proxy.net/", timeout=5)
-            part = str(r.text)
-            part = part.split("<tbody>")
-            part = part[1].split("</tbody>")
-            part = part[0].split("<tr><td>")
+            part = str(r.text).split("<tbody>")[1].split("</tbody>")[0].split("<tr><td>")
             proxies = ""
             for proxy in part:
                 proxy = proxy.split("</td><td>")
                 try:
                     if proxies != '':
-                        proxies = proxies + proxy[0] + ":" + proxy[1] + "\n"
+                        proxies += proxy[0] + ":" + proxy[1] + "\n"
                         if proxies != '':
                             proxy_list += proxies.split('\n')
                 except:
                     pass
-
             return proxy_list
         except:
             return []
 
     def get_special2(self):
-        for i in range(json.loads(requests.get('https://proxylist.geonode.com/api/proxy-summary').text)["summary"]['proxiesOnline'] // 100):
-            proxies = json.loads(requests.get('https://proxylist.geonode.com/api/proxy-list?limit=100&page={}&sort_by=lastChecked&sort_type=desc'.format(i)).text)
-            for p in proxies['data']:
-                if p['protocols'][0] == 'https':
-                    protocol = 'http'
-                else:
-                    protocol = p['protocols'][0]
-                self.proxy_dict[protocol].append('{}:{}'.format(p['ip'], p['port']))
-        return
+        try:
+            summary = json.loads(requests.get('https://proxylist.geonode.com/api/proxy-summary').text)
+            for i in range(summary["summary"]['proxiesOnline'] // 100):
+                proxies = json.loads(requests.get(
+                    'https://proxylist.geonode.com/api/proxy-list?limit=100&page={}&sort_by=lastChecked&sort_type=desc'.format(i)).text)
+                for p in proxies['data']:
+                    protocol = 'http' if p['protocols'][0] == 'https' else p['protocols'][0]
+                    self.proxy_dict[protocol].append('{}:{}'.format(p['ip'], p['port']))
+        except:
+            pass
 
     def get(self):
         self.proxy_dict['socks4'] += self.get_special1()
-        # self.get_special2()
         self.get_extra()
 
         for type in ['socks4', 'socks5', 'http']:
@@ -91,37 +88,38 @@ class Downloadproxies():
         print('> Get {} proxies done'.format(type))
 
     def get_extra(self):
+        retries = 5
         for q in range(20):
             self.count = {'http': 0, 'socks5': 0}
             self.day = datetime.date.today() + datetime.timedelta(-q)
-            self.r = requests.get('https://checkerproxy.net/api/archive/{}-{}-{}'.format(self.day.year, self.day.month, self.day.day))
-
-            # Vérification si la réponse est vide ou non valide
-            if self.r.status_code == 200 and self.r.text.strip():
+            url = 'https://checkerproxy.net/api/archive/{}-{}-{}'.format(self.day.year, self.day.month, self.day.day)
+            for i in range(retries):
                 try:
+                    self.r = requests.get(url, timeout=10)
+                    self.r.raise_for_status()
                     self.json_result = json.loads(self.r.text)
-                except json.JSONDecodeError:
-                    print(f"Erreur de décodage JSON pour la date {self.day}. Réponse: {self.r.text}")
-                    continue  # Passer à l'itération suivante en cas d'erreur JSON
-            else:
-                print(f"Aucune donnée reçue pour la date {self.day}. Réponse: {self.r.text}")
-                continue
-
-            for i in self.json_result:
-                if re.match(r'172\.[0-9][0-9]\.0\.1', i['ip']):
-                    if i['type'] in [1, 2] and i['addr'] in self.proxy_dict['http']:
-                        self.proxy_dict['http'].remove(i['addr'])
-                    if i['type'] == 4 and i['addr'] in self.proxy_dict['socks5']:
-                        self.proxy_dict['socks5'].remove(i['addr'])
-                else:
-                    if i['type'] in [1, 2]:
-                        self.count['http'] += 1
-                        self.proxy_dict['http'].append(i['addr'])
-                    if i['type'] == 4:
-                        self.count['socks5'] += 1
-                        self.proxy_dict['socks5'].append(i['addr'])
-            print('> Get {} http proxy ips from {}'.format(self.count['http'], self.r.url))
-            print('> Get {} socks5 proxy ips from {}'.format(self.count['socks5'], self.r.url))
+                    for i in self.json_result:
+                        if re.match(r'172\.\d{1,3}\.0\.1', i['ip']):
+                            if i['type'] in [1, 2] and i['addr'] in self.proxy_dict['http']:
+                                self.proxy_dict['http'].remove(i['addr'])
+                            if i['type'] == 4 and i['addr'] in self.proxy_dict['socks5']:
+                                self.proxy_dict['socks5'].remove(i['addr'])
+                        else:
+                            if i['type'] in [1, 2]:
+                                self.count['http'] += 1
+                                self.proxy_dict['http'].append(i['addr'])
+                            if i['type'] == 4:
+                                self.count['socks5'] += 1
+                                self.proxy_dict['socks5'].append(i['addr'])
+                    print('> Get {} http proxy ips from {}'.format(self.count['http'], self.r.url))
+                    print('> Get {} socks5 proxy ips from {}'.format(self.count['socks5'], self.r.url))
+                    break
+                except (ConnectionError, HTTPError) as e:
+                    print(f"Tentative {i + 1} échouée: {e}")
+                    if i < retries - 1:
+                        time.sleep(5)
+                    else:
+                        raise
 
         self.proxy_dict['socks4'] = list(set(self.proxy_dict['socks4']))
         self.proxy_dict['socks5'] = list(set(self.proxy_dict['socks5']))
@@ -135,9 +133,7 @@ class Downloadproxies():
             self.out_file = 'proxies/{}.txt'.format(type)
             with open(self.out_file, 'w') as f:
                 for i in self.proxy_dict[type]:
-                    if '#' in i or i == '\n':
-                        self.proxy_dict[type].remove(i)
-                    else:
+                    if '#' not in i and i != '\n':
                         f.write(i + '\n')
             print("> Have already saved {} proxies list as ".format(len(self.proxy_dict[type])) + self.out_file)
 
@@ -148,8 +144,9 @@ class Downloadproxies():
                     all_file.write(proxy + '\n')
         print("> Have already saved all proxies list as proxies/all.txt")
 
+
 if __name__ == '__main__':
-    d = Downloadproxies()
+    d = DownloadProxies()
     d.get()
     d.save()
     d.save_all()
